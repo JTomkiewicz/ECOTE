d2702ddbd04ea05ea3b608c18c287ca764ad1c34
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-05-09 21:12:53 +0200

    regex_dfa.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import menu
+import regex_dfa
 
 
 def main():
     option = menu.init()
+
+    if option == 1:
+        regex = input('Input REGEX: ')
+        regex_dfa.to_dfa(regex)
+    elif option == 2:
+        regex = input('Input REGEX: ')
+    else:
+        print('Test 1: (a|b)*abb')
+        regex_dfa.to_dfa('(a|b)*abb')
 
 
 if __name__ == "__main__":
---
+++ regex_dfa.py
@@ -0,0 +1,2 @@
+def to_dfa(regex: str):
+    print('1')
