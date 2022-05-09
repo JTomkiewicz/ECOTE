a94f5136cb2f9e19219d847bcd3dd46d2af69472
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-05-09 22:07:00 +0200

    fix option numbers

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ import regex_dfa
 def main():
     option = menu.init()
 
-    if option == 1:
+    if option == 0:
         regex = input('Input REGEX: ')
         regex_dfa.to_dfa(regex)
-    elif option == 2:
+    elif option == 1:
         regex = input('Input REGEX: ')
     else:
         print('Test 1: (a|b)*abb')
--- regex_dfa.py
+++ regex_dfa.py
@@ -1,2 +1,8 @@
-def to_dfa(regex: str):
+def is_regex_correct(regex: str):
     print('1')
+
+
+def to_dfa(regex: str):
+    if not is_regex_correct(regex):
+        print('Given REGEX is incorrect')
+        quit()
