606e4371ee1c8d90a5ce5e4a3fd6ba5fa7903c6f
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-05-22 16:45:36 +0200

    is regex correct

--- constructor.py
+++ constructor.py
@@ -1,11 +1,28 @@
 import re
 
-
-def is_regex_correct(regex: str):
-    print('1')
+from sympy import false
 
 
-def remove_not_supported_chars(regex: str):
+def is_regex_correct(regex: str) -> bool:
+    open_sum = 0
+    close_sum = 0
+
+    for char in regex:
+        if char == '(':
+            open_sum += 1
+        elif char == ')':
+            close_sum += 1
+
+        if close_sum > open_sum:
+            return False
+
+    if open_sum != close_sum:
+        return False
+    else:
+        return True
+
+
+def remove_not_supported_chars(regex: str) -> str:
     regex = re.sub('[^a-zA-Z0-9\*\(\)\|\+]', '', regex)
     return regex
 
