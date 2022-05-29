574426e9e3f2e51de1d66013bd657a9c28f039f0
Jakub Tomkiewicz <kubatomki@gmail.com>
2022-05-29 17:28:30 +0200

    build_tree

--- constructor.py
+++ constructor.py
@@ -19,8 +19,15 @@ class SyntaxTree:
         self.create_tokens(regex)
         self.create_stack()
 
+        self.root = Node('.', 'concat')
+        self.leaves = dict()
+        self.id_counter = 1
+
+        self.build_tree()
+
     def create_tokens(self, regex):
         temp_str = ''
+
         for char in regex:
             if char in ['(', ')', '|', '*', '+', '.']:
                 if temp_str != '':
@@ -29,11 +36,13 @@ class SyntaxTree:
                 self.tokens.append(char)
             else:
                 temp_str += char
+
         if temp_str != '':
             self.tokens.append(temp_str)
 
     def create_stack(self):
         temp_stack = []
+
         for token in self.tokens:
             if token == '(':
                 temp_stack.append(token)
@@ -55,5 +64,40 @@ class SyntaxTree:
                 temp_stack.append(token)
             else:
                 self.stack.append(token)
+
         while len(temp_stack) > 0:
             self.stack.append(temp_stack.pop())
+
+    def build_tree(self):
+        temp_stack = []
+
+        for token in self.stack:
+            if token == '.':
+                lc = temp_stack.pop()
+                rc = temp_stack.pop()
+                temp_stack.append(
+                    Node('.', 'concat', lchild=lc, rchild=rc))
+            elif token == '*':
+                lc = temp_stack.pop()
+                temp_stack.append(Node('*', 'star', lchild=lc))
+            elif token == '|':
+                lc = temp_stack.pop()
+                rc = temp_stack.pop()
+                temp_stack.append(Node('|', 'or', lchild=lc, rchild=rc))
+            elif token == '+':
+                lc = temp_stack.pop()
+                temp_stack.append(Node('*', 'plus', lchild=lc))
+            else:
+                temp_node = Node(token, 'identifier', id=self.next_id())
+                self.leaves[temp_node.id] = temp_node.label
+                temp_stack.append(temp_node)
+
+        temp_node = Node('#', 'identifier', id=self.next_id())
+        self.leaves[temp_node.id] = temp_node.label
+        self.root.lchild = temp_stack.pop()
+        self.root.rchild = temp_node
+
+    def next_id(self):
+        i = self.id_counter
+        self.id_counter += 1
+        return i
